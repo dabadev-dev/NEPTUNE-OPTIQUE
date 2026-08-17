49b9c7c6d81c5e28ad1b7a64682bec50124f2f60
Daba Gueye <11gueyedaba10@gmail.com>
2026-08-17 17:12:48 +0200

    changement

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ function App() {
     <div>
       <h1 className="bg-red-500">bonjour daba</h1>
       <h1 className="bg-purple-500 font-bold">bonjour abdoulay</h1>
+      <h1 className="bg-red-500 font-extrabold">bonjour daba</h1>
+
 
     </div>
   )
