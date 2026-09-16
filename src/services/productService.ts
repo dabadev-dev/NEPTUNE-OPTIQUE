cfcf8a93bea229694a700cf1a131f384376c429e
Daba Gueye <11gueyedaba10@gmail.com>
2026-09-16 17:39:40 +0200

    latyyyyrfvfdgedfdfethrgcgfviyhvuvjb

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -3,6 +3,12 @@ import type { Product } from "../data/products";
 
 
 export const getProducts = async (): Promise<Product[]> => {
-  const response = await axios.get<Product[]>(`http://localhost:3000/products`);
+  const response = await axios.get<Product[]>(`https://backend-neptune.onrender.com/products`);
   return response.data;
 };
+
+
+// export const getProducts = async (): Promise<Product[]> => {
+//   const response = await axios.get<Product[]>(`http://localhost:3000/products`);
+//   return response.data;
+// };
